4d72ab67e8863a3a3356d3299e0d5a34d3030fd1
DogPawDev <foodev@naver.com>
2020-08-30 14:32:39 +0900

    계정 도메인 추가

---
+++ src/main/java/com/board/foodev/domain/Account.java
@@ -0,0 +1,59 @@
+package com.board.foodev.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+//컴파일 키가 없으면 에러가 나타남
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String loaction;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreateByEmail;
+
+    private boolean studyCreateByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+
+
+
+
+
+
+}
