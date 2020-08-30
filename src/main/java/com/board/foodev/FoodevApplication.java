a7a64573c356b18ecc35d77cd2ba40582a2f5e61
DogPawDev <foodev@naver.com>
2020-08-30 14:19:47 +0900

    init

---
+++ src/main/java/com/board/foodev/FoodevApplication.java
@@ -0,0 +1,13 @@
+package com.board.foodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/foodev/FoodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.foodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
